01e34b2c78a9e3b647cd7ea0aee09b501b8da0af
CosmoXxX <50768054+CosmoXxX@users.noreply.github.com>
2019-10-10 18:39:27 +0400

    Update mcskin.js

--- commands/mcskin.js
+++ commands/mcskin.js
@@ -2,7 +2,7 @@
   module.exports.run = (client, message, args) => {
     const name = args.join(" ").filter(a => ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," ","1","2","3","4","5","6","7","8","9"].includes(a.toLowerCase())).join("");
     if (!name) return message.reply("Veuillez saisir le nom d'un skin!");
-    if(name.length < 1) return message.reply("Le pseudo est trop court!");
+    //if(name.length < 1) return message.reply("Le pseudo est trop court!");
     const { MessageEmbed } = require("discord.js");
     const embed = new MessageEmbed()
     .setColor("#0040ff")
