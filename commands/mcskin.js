044a3e90c0a783844ee040015c90cfe66cb17799
CosmoXxX <50768054+CosmoXxX@users.noreply.github.com>
2019-10-10 18:38:25 +0400

    Update mcskin.js

--- commands/mcskin.js
+++ commands/mcskin.js
@@ -1,6 +1,6 @@
 
   module.exports.run = (client, message, args) => {
-    const name = args.join(" ");
+    const name = args.join(" ").filter(a => ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," ","1","2","3","4","5","6","7","8","9"].includes(a.toLowerCase())).join("");
     if (!name) return message.reply("Veuillez saisir le nom d'un skin!");
     if(name.length < 1) return message.reply("Le pseudo est trop court!");
     const { MessageEmbed } = require("discord.js");
@@ -9,7 +9,7 @@
       .setTitle(`**Télécharger le skin de ${name}**`)
       .setURL(`https://minotar.net/download/${name}`)
       .setImage(`https://minotar.net/body/${name}/100.png`);
-    message.channel.send(embed);
+    message.channel.send(embed).catch(err => message.reply("Skin Minecraft introuvable");
   }
 module.exports.config = {
     name: "mcskin",
