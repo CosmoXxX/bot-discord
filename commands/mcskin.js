6ec85916aff0ff7ce1f307d59e45e9e60a176900
CosmoXxX <50768054+CosmoXxX@users.noreply.github.com>
2019-10-11 15:50:55 +0400

    Update mcskin.js

--- commands/mcskin.js
+++ commands/mcskin.js
@@ -1,14 +1,23 @@
 
-  module.exports.run = (client, message, args) => {
-    const name = args.join(" ")split("").toString().replace(/[!@#\$%\^&\*\(\)\[\]\?\:;'",\.\\\/]/gi, "")
-if (!name) return message.reply("Veuillez saisir le nom d'un skin!");
+  module.exports.run = (client, message) => {
+    //const name = args.join(" ")split("").toString().replace(/[!@#\$%\^&\*\(\)\[\]\?\:;'",\.\\\/]/gi, "")
+var args = message.content.split(" ").slice(1).toString().split("")
+var test = "" 
+var test2 = "abcdefghijklmnopqrstuvwxyz1234567890" 
+for(var i = 0; i<args.length ; i++) {
+if(test2.includes(args[i].toLowerCase())) {
+test = test+args[i] 
+} 
+}
+//message.channel.send(test)
+if (!args) return message.reply("Veuillez saisir le nom d'un skin!");
     //if(name.length < 1) return message.reply("Le pseudo est trop court!");
     const { MessageEmbed } = require("discord.js");
     const embed = new MessageEmbed()
     .setColor("#0040ff")
-      .setTitle(`**Télécharger le skin de ${name}**`)
-      .setURL(`https://minotar.net/download/${name}`)
-      .setImage(`https://minotar.net/body/${name}/100.png`);
+      .setTitle(`**Télécharger le skin de ${test}**`)
+      .setURL(`https://minotar.net/download/${test}`)
+      .setImage(`https://minotar.net/body/${test}/100.png`);
     message.channel.send(embed).catch(err => message.reply("Skin Minecraft introuvable"));
   }
 module.exports.config = {
