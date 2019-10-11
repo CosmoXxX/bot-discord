1ac83d796456fdc9031c98070a954baf7ff92559
CosmoXxX <50768054+CosmoXxX@users.noreply.github.com>
2019-10-11 12:42:06 +0400

    Update vs.js

--- commands/vs.js
+++ commands/vs.js
@@ -5,11 +5,12 @@ module.exports.run = async (client, message, args) => {
   let av = use.displayAvatarURL({
   size: 1024,
   format : "png"
-})
+});
   let ava = user.displayAvatarURL({
   size: 1024,
   format : "png"
-})
+});
+if(!use) return message.reply("Veuillez mentionner une personne!");
   const ameClient = require("amethyste-api");
   const ameApi = new ameClient(
     "ff41a095bc493607d2865295c74cf72329fa9681b5fb57c08bae6493d550d7bc3c0433508e04dfd3f2c569d888b8ae33e20529fb1489b6b2e25f92226b6ef1d2"
